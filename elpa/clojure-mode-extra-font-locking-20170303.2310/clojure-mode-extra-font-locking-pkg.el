cb79b47c74af4392a89a5b50040644934476484a
Jon <jonathantow8@gmail.com>
2018-02-12 21:21:22 -0500

    delete elpa junk

--- elpa/clojure-mode-extra-font-locking-20170303.2310/clojure-mode-extra-font-locking-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "clojure-mode-extra-font-locking" "20170303.2310" "Extra font-locking for Clojure mode" '((clojure-mode "3.0")) :commit "eacd330f28513276da18e4d6f2607f9041314c49" :url "http://github.com/clojure-emacs/clojure-mode" :keywords '("languages" "lisp"))
--- elpa/ox-latex-chinese-20170401.439/ox-latex-chinese-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ox-latex-chinese" "20170401.439" "An example org latex exporter configure for Chinese users" '((emacs "24.4") (cl-lib "0.5")) :commit "f79d25443987a169a1234f5fbb2bc115cccea33e" :url "https://github.com/tumashu/ox-latex-chinese")
